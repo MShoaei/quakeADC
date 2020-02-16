c7844aca3a919ee35d186e2cf0a35badc2133c7b
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2020-02-16 22:01:36 +0330

    huge code refactor. moved certain part into separate functions. now gracefully shutdown on CTRL+C optimized I/O by using bufio and string builder

--- rpiCMD/cmd/read.go
+++ rpiCMD/cmd/read.go
@@ -1,15 +1,19 @@
 package cmd
 
 import (
+	"bufio"
 	"encoding/binary"
-	"fmt"
+	"log"
 	"os"
+	"os/signal"
 	"strings"
 	"time"
 
 	"github.com/google/gopacket/pcap"
 	"github.com/spf13/cobra"
 )
+
+var defaultBuilder = strings.Builder{}
 
 type readOptions struct {
 	file     *os.File
@@ -24,7 +28,7 @@ var adcReadCmd = &cobra.Command{
 	Short: "Start Reading from RPI_INTERFACE",
 	// Long:  "Start Reading from RPI_INTERFACE",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f, err := os.OpenFile("access", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			return err
 		}
@@ -33,155 +37,185 @@ var adcReadCmd = &cobra.Command{
 }
 
 func read(opt readOptions) error {
-	// dataFile := opt.file
+	var skip *time.Ticker
 
-	dev := os.Getenv("RPI_INTERFACE")
-	if dev == "" {
-		return fmt.Errorf("RPI_INTERFACE not set")
-	}
-
-	handle, err := pcap.OpenLive(dev, 256, true, pcap.BlockForever)
-	if err != nil {
-		return err
-	}
 	ch := make(chan []byte, 1000000)
-	epoch := time.Now()
-	go func(ch chan<- []byte, d time.Duration) {
-		if opt.duration != 0 {
-			t := time.NewTimer(d * time.Millisecond)
-			for {
-				select {
-				case <-t.C:
-					close(ch)
-					t.Stop()
-					return
-				default:
-					packet, info, err := handle.ReadPacketData()
-					if err != nil {
-						return
-					}
-
-					// 8 byte time in milliseconds
-					buf := make([]byte, 8)
-					binary.LittleEndian.PutUint64(buf, uint64(info.Timestamp.Sub(epoch).Milliseconds()))
-					packet = append(buf, packet...)
-					ch <- packet
-				}
+	writer := bufio.NewWriterSize(opt.file, 10240)
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+	go func() {
+		for sig := range ch {
+			log.Println(sig)
+			err := writer.Flush()
+			if err != nil {
+				log.Println(err)
 			}
-		} else {
-			for {
-				packet, info, err := handle.ReadPacketData()
-				if err != nil {
-					return
-				}
-
-				//8 byte time in milliseconds
-				buf := make([]byte, 8)
-				binary.LittleEndian.PutUint64(buf, uint64(info.Timestamp.Sub(epoch).Milliseconds()))
-				packet = append(buf, packet...)
-				ch <- packet
-			}
+			os.Exit(1)
 		}
-	}(ch, time.Duration(opt.duration))
+	}()
 
 	if opt.ch != nil {
-		skip := time.NewTicker(time.Duration(opt.skip) * time.Millisecond)
-		b := strings.Builder{}
-		b.Grow(100)
-		for {
-			var signCH0 uint32 = 0
-			var signCH1 uint32 = 0
-			var signCH2 uint32 = 0
-			var signCH3 uint32 = 0
-			var packet []byte
-			var ok bool
+		skip = time.NewTicker(time.Duration(opt.skip) * time.Millisecond)
+	}
+	go getPackets(ch, time.Duration(opt.duration))
+	for {
+		var packet []byte
+		var ok bool
 
-			packet, ok = <-ch
-			if !ok {
-				close(opt.ch)
-				dataFile.Close()
-				return nil
+		packet, ok = <-ch
+		if !ok {
+			//close(opt.ch)
+			err := writer.Flush()
+			if err != nil {
+				log.Println(err)
 			}
-			//adcNum := 1 // TODO: should change!
+			dataFile.Close()
+			return nil
+		}
 
-			_ = packet[:8]
-			if int8(packet[24]) < 0 {
-				signCH0 = 255 << 24
-			}
-			ch0Value := (uint32(packet[24]) << 16) + (uint32(packet[25]) << 8) + (uint32(packet[26]))
-
-			if int8(packet[28]) < 0 {
-				signCH1 = 255 << 24
-			}
-			ch1Value := (uint32(packet[28]) << 16) + (uint32(packet[29]) << 8) + uint32(packet[30])
-
-			if int8(packet[32]) < 0 {
-				signCH2 = 255 << 24
-			}
-			ch2Value := (uint32(packet[32]) << 16) + (uint32(packet[33]) << 8) + (uint32(packet[34]))
-
-			if int8(packet[36]) < 0 {
-				signCH3 = 255 << 24
-			}
-			ch3Value := (uint32(packet[36]) << 16) + (uint32(packet[37]) << 8) + (uint32(packet[38]))
-
-			//b.WriteString(fmt.Sprintf("%d,%d,%d,%d,%d,%d\n", t, adcNum, ch0Value, ch1Value, ch2Value, ch3Value))
-			b.WriteString(fmt.Sprintf("%d,%d,%d,%d,%d,%d,%d,%d\n",
-				ch0Value, int32(ch0Value+signCH0),
-				ch1Value, int32(ch1Value+signCH1),
-				ch2Value, int32(ch2Value+signCH2),
-				ch3Value, int32(ch3Value+signCH3)))
-
-			fmt.Fprintf(dataFile, b.String())
+		err := getPacketData(packet)
+		if err != nil {
+			log.Println(err)
+		}
+		writer.WriteString(defaultBuilder.String())
+		if skip != nil {
 			select {
 			case <-skip.C:
-				opt.ch <- b.String()
-				b.Reset()
+				opt.ch <- defaultBuilder.String()
+				defaultBuilder.Reset()
 			default:
-				b.Reset()
-
+				continue
 			}
-		}
-	} else {
-		for {
-			var signCH0 uint32 = 0
-			var signCH1 int32 = 0
-			var signCH2 int32 = 0
-			var signCH3 int32 = 0
-
-			packet := <-ch
-
-			_ = 1 // TODO: should change!
-
-			_ = packet[:8]
-			if int8(packet[24]) < 0 {
-				signCH0 = 255 << 24
-			}
-			ch0Value := signCH0 + (uint32(packet[24]) << 16) + (uint32(packet[25]) << 8) + (uint32(packet[26]))
-
-			if int8(packet[28]) < 0 {
-				signCH1 = -1 << 24
-			}
-			_ = signCH1 + (int32(packet[28]) << 16) + (int32(packet[29]) << 8) + int32(packet[30])
-
-			if int8(packet[32]) < 0 {
-				signCH2 = -1 << 24
-			}
-			_ = signCH2 + (int32(packet[32]) << 16) + (int32(packet[33]) << 8) + (int32(packet[34]))
-
-			if int8(packet[36]) < 0 {
-				signCH3 = -1 << 24
-			}
-			_ = signCH3 + (int32(packet[36]) << 16) + (int32(packet[37]) << 8) + (int32(packet[38]))
-
-			data := fmt.Sprintf("%+d\n", int32(ch0Value))
-			fmt.Fprintf(dataFile, data)
-
-			fmt.Println(data)
 		}
 	}
 }
 
+func getPackets(ch chan<- []byte, d time.Duration) {
+	if d != 0 {
+		getWithTicker(ch, d)
+	} else {
+		getNonStop(ch)
+	}
+}
+
+// getWithTicker reads packets from handle for certain Duration d.
+func getWithTicker(ch chan<- []byte, d time.Duration) {
+	dev := os.Getenv("RPI_INTERFACE")
+	if dev == "" {
+		panic("RPI_INTERFACE not set")
+	}
+
+	p, err := pcap.NewInactiveHandle(dev)
+	if err != nil {
+		panic(err)
+	}
+	err = p.SetPromisc(true)
+	if err != nil {
+		panic(err)
+	}
+	err = p.SetSnapLen(256)
+	if err != nil {
+		panic(err)
+	}
+	err = p.SetTimeout(d)
+	if err != nil {
+		panic(err)
+	}
+
+	handle, err := p.Activate()
+	if err != nil {
+		panic(err)
+	}
+
+	epoch := time.Now()
+	t := time.NewTimer(d * time.Millisecond)
+	for {
+		select {
+		case <-t.C:
+			close(ch)
+			t.Stop()
+			return
+		default:
+			packet, info, err := handle.ReadPacketData()
+			if err != nil {
+				panic(err)
+			}
+
+			// 8 byte time in milliseconds
+			buf := make([]byte, 8)
+			binary.LittleEndian.PutUint64(buf, uint64(info.Timestamp.Sub(epoch).Milliseconds()))
+			packet = append(buf, packet...)
+			ch <- packet
+		}
+	}
+}
+
+func getNonStop(ch chan<- []byte) {
+	dev := os.Getenv("RPI_INTERFACE")
+	if dev == "" {
+		panic("RPI_INTERFACE not set")
+	}
+
+	handle, err := pcap.OpenLive(dev, 256, true, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
+
+	epoch := time.Now()
+	for {
+		packet, info, err := handle.ReadPacketData()
+		if err != nil {
+			panic(err)
+		}
+
+		//8 byte time in milliseconds
+		buf := make([]byte, 8)
+		binary.LittleEndian.PutUint64(buf, uint64(info.Timestamp.Sub(epoch).Milliseconds()))
+		packet = append(buf, packet...)
+		ch <- packet
+	}
+}
+
+func getPacketData(packet []byte) error {
+	var signCH0 uint32 = 0
+	var signCH1 uint32 = 0
+	var signCH2 uint32 = 0
+	var signCH3 uint32 = 0
+
+	defaultBuilder.Reset()
+
+	_ = 1 // adcNum // TODO: should change!
+
+	_ = packet[:8] // time
+	if int8(packet[24]) < 0 {
+		signCH0 = 255 << 24
+	}
+	defaultBuilder.WriteString(string(signCH0 + (uint32(packet[24]) << 16) + (uint32(packet[25]) << 8) + (uint32(packet[26]))))
+	defaultBuilder.WriteString(",")
+
+	if int8(packet[28]) < 0 {
+		signCH1 = 255 << 24
+	}
+	defaultBuilder.WriteString(string(signCH1 + (uint32(packet[28]) << 16) + (uint32(packet[29]) << 8) + (uint32(packet[30]))))
+	defaultBuilder.WriteString(",")
+
+	if int8(packet[32]) < 0 {
+		signCH2 = 255 << 24
+	}
+	defaultBuilder.WriteString(string(signCH2 + (uint32(packet[32]) << 16) + (uint32(packet[33]) << 8) + (uint32(packet[34]))))
+	defaultBuilder.WriteString(",")
+
+	if int8(packet[36]) < 0 {
+		signCH3 = 255 << 24
+	}
+	defaultBuilder.WriteString(string(signCH3 + (uint32(packet[36]) << 16) + (uint32(packet[37]) << 8) + (uint32(packet[38]))))
+	defaultBuilder.WriteString("\r\n")
+
+	return nil
+}
+
 func init() {
 	rootCmd.AddCommand(adcReadCmd)
+	defaultBuilder.Grow(256)
+	defaultBuilder.Reset()
 }
