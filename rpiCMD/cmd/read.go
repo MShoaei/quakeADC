b6733c17519ce373fca5fda0add51825d35cb1a6
Mohammad Shoaei <ms@pop-os.localdomain>
2020-09-21 21:08:13 +0330

    read all 4 channels with better performance.

--- rpiCMD/cmd/read.go
+++ rpiCMD/cmd/read.go
@@ -74,14 +74,13 @@ func read(inFile io.ReadCloser, outFile io.WriteCloser) {
 	buf, _ := ioutil.ReadAll(inFile)
 	dclkChan := clockIndex(buf)
 	for i := range dclkChan {
-		if buf[i]&logic1DataReadyMask == 0 {
-			continue
-		}
-		for buf[i]&logic1DataReadyMask > 0 {
+		for c := 0; c < 4; c++ {
 			for x := 0; x < 8; x++ { //skip 8 bits of header
 				_ = <-dclkChan
 			}
+
 			i = <-dclkChan // bit 23 of data
+
 			data[0], data[1], data[2], data[3], data[4] = 0, 0, 0, 0, 0
 			if buf[i]&logic1DataOut1Mask > 0 {
 				data[0] = 255 << 24
@@ -115,32 +114,76 @@ func read(inFile io.ReadCloser, outFile io.WriteCloser) {
 					strconv.FormatInt(int64(int32(data[1])), 10) + "," +
 					strconv.FormatInt(int64(int32(data[2])), 10) + "," +
 					strconv.FormatInt(int64(int32(data[3])), 10) + "," +
-					strconv.FormatInt(int64(int32(data[4])), 10) + "\n")
+					strconv.FormatInt(int64(int32(data[4])), 10) + ",")
 			//outFile.Write([]byte(fmt.Sprintf("%024b\n",int64(int32(data[0])))))
 		}
+		defaultWriter.WriteByte('\n')
 	}
 	defaultWriter.Flush()
 }
 
 func clockIndex(buf []byte) (dclkChan <-chan int) {
-	dclk := make(chan int, 20000)
-	go func() {
-		temp := [2]bool{
+	dclk := make(chan int, 1000)
+	drdyChan := dataReadyIndex(buf)
+	go func(drdyChan <-chan int) {
+		tempClock := [2]bool{
 			buf[0]&logic1DataClockMask > 0,
 			buf[1]&logic1DataClockMask == 0,
 		}
-		//_ = buf[len(buf)-1]
-		for i := 0; i < len(buf)-1; i++ {
-			temp[0] = !temp[1]
-			temp[1] = buf[i+1]&logic1DataClockMask == 0
-			if !(temp[0] && temp[1]) {
-				continue
-			}
+
+		for i := range drdyChan {
+
+			// without this the first for loop inside read function won't work correctly
+			// maybe there is a cleaner way to fix this?
 			dclk <- i + 1
+
+			for clock := 0; clock < 32*4; {
+				tempClock[0] = !tempClock[1]
+				tempClock[1] = buf[i+1]&logic1DataClockMask == 0
+				if !(tempClock[0] && tempClock[1]) {
+					i++
+					continue
+				}
+				dclk <- i + 1
+				clock++
+			}
 		}
 		close(dclk)
-	}()
+	}(drdyChan)
 	return dclk
+}
+
+func dataReadyIndex(buf []byte) (drdyChan <-chan int) {
+	drdy := make(chan int, 1000)
+	go func() {
+		tempDataReady := []bool{
+			buf[0]&logic1DataReadyMask > 0,
+			buf[1]&logic1DataReadyMask == 0,
+		}
+
+		tempSync := []uint8{
+			buf[0]&logic1SyncMask,
+			buf[1]&logic1SyncMask,
+		}
+		i := 0
+		for ; i < len(buf)-1; i++ {
+			tempSync[0] = tempSync[1]
+			tempSync[1] = buf[i+1]&logic1SyncMask
+			if tempSync[0] == 0 && tempSync[1] == 8 {
+				break
+			}
+		}
+		for ; i < len(buf)-1; i++ {
+			tempDataReady[0] = !tempDataReady[1]
+			tempDataReady[1] = buf[i+1]&logic1DataReadyMask == 0
+			if !(tempDataReady[0] && tempDataReady[1]) {
+				continue
+			}
+			drdy <- i
+		}
+		close(drdy)
+	}()
+	return drdy
 }
 
 func init() {
