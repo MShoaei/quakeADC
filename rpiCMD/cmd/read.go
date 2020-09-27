9d4e550a68eb5b32ed3fd4386c9d8c041c26bfc7
Mohammad Shoaei <ms@pop-os.localdomain>
2020-09-27 19:09:44 +0330

    major refactor

--- rpiCMD/cmd/read.go
+++ rpiCMD/cmd/read.go
@@ -3,9 +3,9 @@ package cmd
 import (
 	"bufio"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"strconv"
 	"strings"
@@ -27,32 +27,81 @@ const (
 var defaultBuilder = strings.Builder{}
 var defaultWriter = bufio.NewWriterSize(nil, 52428800)
 
-// readCmd represents the read command
-var adcReadCmd = &cobra.Command{
-	Use:   "readAll",
-	Short: "Start Reading from data file",
+// adcConvertCmd represents the convert command
+var adcConvertCmd = &cobra.Command{
+	Use:   "convert",
+	Short: "read data from 'if', convert the data and write to 'of'",
 	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			i   io.ReadCloser
+			o   io.WriteCloser
+			err error
+		)
+
+		// c := libcmd.NewCmd("sigrok-cli", args...)
+
 		input, _ := cmd.Flags().GetString("if")
-		i, err := os.Open(input)
-		if err != nil {
-			log.Fatalln(err)
+		if input == "" {
+			i = os.Stdin
+		} else {
+			i, err = os.Open(input)
+			if err != nil {
+				log.Fatalln(err)
+			}
 		}
 
 		output, _ := cmd.Flags().GetString("of")
 		if output == "" {
-			read(i, os.Stdout)
-			return
+			o = os.Stdout
+		} else {
+			o, err = os.OpenFile(output, os.O_RDONLY|os.O_CREATE|os.O_TRUNC, 0666)
+			if err != nil {
+				log.Fatalln(err)
+			}
 		}
-
-		o, err := os.OpenFile(output, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		read(i, o)
+		convert(i, o)
 	},
 }
 
-func read(inFile io.ReadCloser, outFile io.WriteCloser) {
+var adcReadCmd = &cobra.Command{
+	Use:   "read",
+	Short: "read data from logic analyzer and write to 'of'",
+	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			i   *os.File
+			o   *os.File
+			err error
+		)
+
+		input, _ := cmd.Flags().GetString("if")
+
+		output, _ := cmd.Flags().GetString("of")
+		if output == "" {
+			o = os.Stdout
+		} else {
+			o, err = os.OpenFile(output, os.O_RDONLY|os.O_CREATE|os.O_TRUNC, 0666)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		c := exec.Command("sigrok-cli", "--driver=fx2lafw", "-O", "binary", "--time", "30s", "-o", input, "--config", "samplerate=24m")
+		if err := c.Start(); err != nil {
+			log.Fatalf("read command sigrok-cli start failed: %v", err)
+		}
+
+		i, err = os.Open(input)
+		if err != nil {
+			log.Fatalf("failed to open file to read: %v", err)
+		}
+
+		convert(i, o)
+		//if err := c.Wait(); err != nil {
+		//	log.Fatal(err)
+		//}
+	},
+}
+
+func convert(inFile io.ReadCloser, outFile io.WriteCloser) {
 	defer outFile.Close()
 	defer inFile.Close()
 
@@ -71,128 +120,118 @@ func read(inFile io.ReadCloser, outFile io.WriteCloser) {
 	}()
 
 	data := make([]uint32, 5, 5)
-	buf, _ := ioutil.ReadAll(inFile)
-	dclkChan := clockIndex(buf)
-	for i := range dclkChan {
+	dclkChan := clockIndex(inFile)
+	for b := range dclkChan {
 		for c := 0; c < 4; c++ {
 			for x := 0; x < 8; x++ { //skip 8 bits of header
 				_ = <-dclkChan
 			}
 
-			i = <-dclkChan // bit 23 of data
+			b = <-dclkChan // bit 23 of data
 
 			data[0], data[1], data[2], data[3], data[4] = 0, 0, 0, 0, 0
-			if buf[i]&logic1DataOut1Mask > 0 {
+			if b&logic1DataOut1Mask > 0 {
 				data[0] = 255 << 24
 			}
-			if buf[i]&logic1DataOut0Mask > 0 {
+			if b&logic1DataOut0Mask > 0 {
 				data[1] = 255 << 24
 			}
-			if buf[i]&logic1DataOut3Mask > 0 {
+			if b&logic1DataOut3Mask > 0 {
 				data[2] = 255 << 24
 			}
-			if buf[i]&logic1DataOut2Mask > 0 {
+			if b&logic1DataOut2Mask > 0 {
 				data[3] = 255 << 24
 			}
-			if buf[i]&logic1DataOut5Mask > 0 {
+			if b&logic1DataOut5Mask > 0 {
 				data[4] = 255 << 24
 			}
 
 			for counter := 23; counter >= 0; counter-- {
-				data[0] |= uint32(buf[i]&logic1DataOut1Mask) >> 5 << counter
-				data[1] |= uint32(buf[i]&logic1DataOut0Mask) >> 4 << counter
-				data[2] |= uint32(buf[i]&logic1DataOut3Mask) >> 2 << counter
-				data[3] |= uint32(buf[i]&logic1DataOut2Mask) >> 1 << counter
-				data[4] |= uint32(buf[i]&logic1DataOut5Mask) >> 0 << counter
+				data[0] |= uint32(b&logic1DataOut1Mask) >> 5 << counter
+				data[1] |= uint32(b&logic1DataOut0Mask) >> 4 << counter
+				data[2] |= uint32(b&logic1DataOut3Mask) >> 2 << counter
+				data[3] |= uint32(b&logic1DataOut2Mask) >> 1 << counter
+				data[4] |= uint32(b&logic1DataOut5Mask) >> 0 << counter
 				if counter > 0 {
-					i = <-dclkChan
+					b = <-dclkChan
 				}
 			}
 
-			defaultWriter.WriteString(
-				strconv.FormatInt(int64(int32(data[0])), 10) + "," +
-					strconv.FormatInt(int64(int32(data[1])), 10) + "," +
-					strconv.FormatInt(int64(int32(data[2])), 10) + "," +
-					strconv.FormatInt(int64(int32(data[3])), 10) + "," +
-					strconv.FormatInt(int64(int32(data[4])), 10) + ",")
-			//outFile.Write([]byte(fmt.Sprintf("%024b\n",int64(int32(data[0])))))
+			line := make([]byte, 0, 500)
+
+			line = strconv.AppendInt(line, int64(int32(data[0])), 10)
+			line = append(line, ',')
+			line = strconv.AppendInt(line, int64(int32(data[1])), 10)
+			line = append(line, ',')
+			line = strconv.AppendInt(line, int64(int32(data[2])), 10)
+			line = append(line, ',')
+			line = strconv.AppendInt(line, int64(int32(data[3])), 10)
+			line = append(line, ',')
+			line = strconv.AppendInt(line, int64(int32(data[4])), 10)
+			line = append(line, ',')
+
+			outFile.Write(line)
 		}
-		defaultWriter.WriteByte('\n')
+		outFile.Write([]byte{'\n'})
 	}
-	defaultWriter.Flush()
+	//defaultWriter.Flush()
 }
 
-func clockIndex(buf []byte) (dclkChan <-chan int) {
-	dclk := make(chan int, 1000)
-	drdyChan := dataReadyIndex(buf)
-	go func(drdyChan <-chan int) {
+func clockIndex(inFile io.Reader) (dclkChan <-chan byte) {
+	dclk := make(chan byte, 1000)
+	buf := make([]byte, 1, 1)
+	go func() {
 		tempClock := [2]bool{
 			buf[0]&logic1DataClockMask > 0,
-			buf[1]&logic1DataClockMask == 0,
+			// buf[1]&logic1DataClockMask == 0,
+			buf[0]&logic1DataClockMask > 0,
+		}
+		tempDataReady := [2]bool{
+			buf[0]&logic1DataReadyMask > 0,
+			// buf[1]&logic1DataReadyMask == 0,
+			buf[0]&logic1DataReadyMask > 0,
 		}
 
-		for i := range drdyChan {
+		for {
+			if _, err := inFile.Read(buf); err != nil {
+				log.Printf("read error: %v", err)
+				break
+			}
+			tempDataReady[0] = !tempDataReady[1]
+			tempDataReady[1] = buf[0]&logic1DataReadyMask == 0
+			if !(tempDataReady[0] && tempDataReady[1]) {
+				continue
+			}
 
-			// without this the first for loop inside read function won't work correctly
+			// without this the first for loop inside convert function won't work correctly
 			// maybe there is a cleaner way to fix this?
-			dclk <- i + 1
+			dclk <- buf[0]
 
 			for clock := 0; clock < 32*4; {
+				inFile.Read(buf)
 				tempClock[0] = !tempClock[1]
-				tempClock[1] = buf[i+1]&logic1DataClockMask == 0
+				tempClock[1] = buf[0]&logic1DataClockMask == 0
 				if !(tempClock[0] && tempClock[1]) {
-					i++
 					continue
 				}
-				dclk <- i + 1
+				dclk <- buf[0]
 				clock++
 			}
 		}
 		close(dclk)
-	}(drdyChan)
+	}()
 	return dclk
 }
 
-func dataReadyIndex(buf []byte) (drdyChan <-chan int) {
-	drdy := make(chan int, 1000)
-	go func() {
-		tempDataReady := []bool{
-			buf[0]&logic1DataReadyMask > 0,
-			buf[1]&logic1DataReadyMask == 0,
-		}
-
-		tempSync := []uint8{
-			buf[0]&logic1SyncMask,
-			buf[1]&logic1SyncMask,
-		}
-		i := 0
-		for ; i < len(buf)-1; i++ {
-			tempSync[0] = tempSync[1]
-			tempSync[1] = buf[i+1]&logic1SyncMask
-			if tempSync[0] == 0 && tempSync[1] == 8 {
-				break
-			}
-		}
-		for ; i < len(buf)-1; i++ {
-			tempDataReady[0] = !tempDataReady[1]
-			tempDataReady[1] = buf[i+1]&logic1DataReadyMask == 0
-			if !(tempDataReady[0] && tempDataReady[1]) {
-				continue
-			}
-			drdy <- i
-		}
-		close(drdy)
-	}()
-	return drdy
-}
-
 func init() {
-	rootCmd.AddCommand(adcReadCmd)
+	rootCmd.AddCommand(adcConvertCmd, adcReadCmd)
+
+	adcConvertCmd.Flags().String("if", "", "the file to read and convert")
+	adcConvertCmd.Flags().String("of", "", "the file to write the result")
 
 	adcReadCmd.Flags().String("if", "", "the file to read and convert")
-	_ = adcReadCmd.MarkFlagRequired("input")
+	_ = adcReadCmd.MarkFlagRequired("if")
 	adcReadCmd.Flags().String("of", "", "the file to write the result")
-	//_ = adcReadCmd.MarkFlagRequired("output")
 
 	defaultBuilder.Grow(256)
 	defaultBuilder.Reset()
